fe1c095787f350cb282b94342c1c5bbf12549ec6
Jon Topper <jon@scalefactory.com>
2014-11-26 10:39:55 +0000

    Changed package version

--- recipe.rb
+++ recipe.rb
@@ -5,16 +5,16 @@ class UA2uaHTTP < FPM::Cookery::Recipe
     description 'uaHTTP server'
     maintainer  'Jon Topper <jon@scalefactory.com>'
     vendor      'fpm'
-    revision    0
+    version     '0.2b7'
 
     depends 'perl-HTTP-Daemon-SSL', 'daemonize'
 
     post_install "dist/post-install"
 
-    if ENV.has_key?('PKG_VERSION')
-        version ENV['PKG_VERSION']
+    if ENV.has_key?('PKG_REVISION')
+        revision ENV['PKG_REVISION']
     else
-        raise 'No PKG_VERSION passed in the environment'
+        raise 'No PKG_REVISION passed in the environment'
     end
 
     def build
